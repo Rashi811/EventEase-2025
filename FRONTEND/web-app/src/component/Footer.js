b353d58d5555fc4a3e3daaff8777349469c2a47d
Rashi811 <rashiniharasara@gmail.com>
2025-03-25 16:17:01 +0530

    AboutUs page created

--- FRONTEND/web-app/src/component/Footer.js
+++ FRONTEND/web-app/src/component/Footer.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { FaYoutube, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa"; // Importing icons
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-icons">
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">📺 YouTube</a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">📘 Facebook</a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">🐦 Twitter</a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">📷 Instagram</a>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <FaYoutube size={30} /> YouTube
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size={30} /> Facebook
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size={30} /> Twitter
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={30} /> Instagram
+        </a>
       </div>
       <p>© 2025 EventEase. All rights reserved.</p>
     </footer>
@@ -16,4 +25,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
